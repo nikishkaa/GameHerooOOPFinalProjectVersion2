f90c718b1aacf4cf20a51c6151f74c851245ee0e
nikishkaa <nikita.goutor@gmail.com>
2024-03-20 13:52:02 +0300

    Add main class. Add user UI class.

---
+++ src/by/itstep/goutor/finaloopproject/controller/Main.java
@@ -0,0 +1,12 @@
+package by.itstep.goutor.finaloopproject.controller;
+
+import by.itstep.goutor.finaloopproject.util.GameHeroUI;
+import by.itstep.goutor.finaloopproject.view.Printer;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Printer.print(GameHeroUI.getUserUI());
+    }
+}
---
+++ src/by/itstep/goutor/finaloopproject/util/GameHeroUI.java
@@ -0,0 +1,14 @@
+package by.itstep.goutor.finaloopproject.util;
+
+public class GameHeroUI {
+    public static final StringBuilder BUILDER;
+
+    static {
+        BUILDER = new StringBuilder();
+    }
+
+    public static StringBuilder getUserUI() {
+
+        return BUILDER;
+    }
+}
