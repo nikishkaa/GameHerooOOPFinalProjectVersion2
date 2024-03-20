47012908711399c659b1c34b1dd33aae31341cbf
nikishkaa <nikita.goutor@gmail.com>
2024-03-20 14:23:13 +0300

    Add magician astrologer

---
+++ src/by/itstep/goutor/finaloopproject/model/entity/MagicianAstrologer.java
@@ -0,0 +1,12 @@
+package by.itstep.goutor.finaloopproject.model.entity;
+
+public class MagicianAstrologer extends {
+    private String name;
+    private boolean alive;
+    private int health;
+    private int level;
+    private int force;
+    private int magicDamage;
+
+
+}
