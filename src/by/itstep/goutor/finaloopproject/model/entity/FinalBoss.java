1ab74c5c320bdf442b705e9de3eac8f26d5efba1
nikishkaa <nikita.goutor@gmail.com>
2024-04-12 23:18:13 +0300

    delite enum.

--- src/by/itstep/goutor/finaloopproject/model/entity/FinalBoss.java
+++
@@ -1,7 +0,0 @@
-package by.itstep.goutor.finaloopproject.model.entity;
-
-public enum FinalBoss {
-    BIG_DRAGON;
-    //Реализовать финального босса через енам тк он должен быть единственным обьектов в отличии от дефолтных боссов
-
-}
